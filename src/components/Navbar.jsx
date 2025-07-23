417e18f882772c68dabdd0522b91ebdda61b7bc1
Egide <tegide2024@gmail.com>
2025-07-23 12:21:54 +0200

    Update Navbar.jsx
    Added beta mark

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
+    <div className="flex">
     <div className="border-2 border-gray-800 text-lg  mt-6 rounded-lg w-3/4 mx-auto flex justify-between p-2">
       <span className="text-gray-500">Egide Tuyishime</span>
       <span className="text-gray-500 ">
@@ -10,6 +11,9 @@ const Navbar = () => {
         <span className="mx-4">Contacts</span>
       </span>
     </div>
+<span className="bg-blue-100 text-blue-600 rounded text-sm font-medium mt-5 leading-[3.5] inline-block">Beta version </span>
+      
+      </div>
   );
 };
 
